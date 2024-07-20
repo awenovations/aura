8412a9614882ddca2334735ec3d9003fd6d7cd46
Benjamin Knox <bknox.contact@gmail.com>
2024-07-20 19:54:40 -0400

    [Divider]: allow all props on main element

--- src/lib/divider/divider.svelte
+++ src/lib/divider/divider.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import classNames from 'classnames';
 	import type * as Props from './props';
 
 	export let direction: Props.DividerDirection = 'horizontal';
@@ -7,63 +8,63 @@
 	const horizontal = direction === 'horizontal';
 </script>
 
-<div class:vertical class:horizontal class="aura-divider">
-  {#if $$slots.default}
-    <div class="text">
-      <slot />
-    </div>
-  {/if}
+<div {...$$props} class:vertical class:horizontal class={classNames($$props.class, 'aura-divider')}>
+	{#if $$slots.default}
+		<div class="text">
+			<slot />
+		</div>
+	{/if}
 </div>
 
 <style lang="scss">
 	.aura-divider {
-    --fill: var(--aura-divider-fill);
-    --border: 1px solid var(--fill);
+		--fill: var(--aura-divider-fill);
+		--border: 1px solid var(--fill);
 
-    & .text {
-      color: var(--fill);
-    }
+		& .text {
+			color: var(--fill);
+		}
 
 		display: flex;
-    flex: 1;
+		flex: 1;
 
-    &:before,
-    &:after {
-      content: '';
-      flex: 1 1;
-      margin: auto;
-    }
+		&:before,
+		&:after {
+			content: '';
+			flex: 1 1;
+			margin: auto;
+		}
 
 		&.horizontal {
 			flex-direction: row;
-      height: min-content;
+			height: min-content;
 
-      & .text {
-        margin: auto 0.625rem;
-        height: min-content;
-      }
+			& .text {
+				margin: auto 0.625rem;
+				height: min-content;
+			}
 
-      &:before,
-      &:after {
-        border-bottom: var(--border);
-        transform: translateY(-0.5px);
-      }
+			&:before,
+			&:after {
+				border-bottom: var(--border);
+				transform: translateY(-0.5px);
+			}
 		}
 
 		&.vertical {
-      height: 100%;
+			height: 100%;
 			flex-direction: column;
 
-      & .text {
-        margin: 0.625rem 0;
-        text-align: center;
-      }
+			& .text {
+				margin: 0.625rem 0;
+				text-align: center;
+			}
 
-      &:before,
-      &:after {
-        border-right: var(--border);
-        transform: translateX(-0.5px);
-      }
+			&:before,
+			&:after {
+				border-right: var(--border);
+				transform: translateX(-0.5px);
+			}
 		}
 	}
 </style>

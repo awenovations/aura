5972485afb604708c2ee2d0fe5eef52947aab7d9
Benjamin Knox <bknox.contact@gmail.com>
2024-08-17 11:51:12 -0400

    [Link]: Allow setting on click event

--- src/lib/link/link.svelte
+++ src/lib/link/link.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
 	import classNames from 'classnames';
+	import { createEventDispatcher } from 'svelte';
 	import type { SvelteHTMLElements } from 'svelte/elements';
 
 	type $$Props = Omit<Partial<SvelteHTMLElements['a']>, 'href'> & {
 		href: string;
 	};
 
+	const dispatch = createEventDispatcher<{
+		click: Event & { target: HTMLAnchorElement };
+	}>();
+
+	function forwardEvent(e: Event) {
+		dispatch(e.type as any, e as Event & { target: HTMLAnchorElement });
+	}
+
 	export let href: string;
 </script>
 
-<a {href} {...$$props} class={classNames($$props.class, 'aura-link')}>
+<a {href} {...$$props} class={classNames($$props.class, 'aura-link')} on:click={forwardEvent}>
 	<slot />
 </a>
 

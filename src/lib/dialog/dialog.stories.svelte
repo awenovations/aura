ce15be0f8218acb100eaeb1103bb3f0e55240ea8
Benjamin Knox <bknox.contact@gmail.com>
2025-02-10 19:11:57 -0500

    [Dialog]: Open and closed

---
+++ src/lib/dialog/dialog.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+	import '../../app.scss';
+	import Dialog from '$lib/dialog/dialog.svelte';
+
+	export const meta = {
+		title: 'AURA/Dialog',
+		component: Dialog,
+		tags: ['autodocs'],
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+
+<Template>
+  <Dialog />
+</Template>
+
+<Story name="Default Dialog" />
---
+++ src/lib/dialog/dialog.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import classNames from 'classnames';
+
+	import Button from '$lib/button/button.svelte';
+	import Container from '$lib/container/container.svelte';
+
+	export let cancelText = 'Cancel';
+	export let confirmText = 'Confirm';
+
+	export let onConfirm: () => void = () => {};
+	export let onCancel: () => void = () => {};
+</script>
+
+<Container clearPadding variant="elevated" class={classNames('dialog', $$restProps.class)}>
+	<slot>Are you sure you wish to take this action?</slot>
+	<div class="action-buttons">
+		<Button size="small" on:click={onCancel}>{cancelText}</Button>
+		<Button size="small" on:click={onConfirm} variant="secondary">{confirmText}</Button>
+	</div>
+</Container>
+
+<style lang="scss">
+	:global(.dialog) {
+		font: var(--aura-default);
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+		padding: 20px;
+	}
+
+	.action-buttons {
+		display: flex;
+		justify-content: flex-end;
+		gap: 8px;
+		transform: translate(12px, 12px);
+	}
+</style>
